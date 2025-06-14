c9c44073d74156fe62593ed4e09ab61ea3917aa1
iseeduck <calicocat40k@gmail.com>
2025-06-14 12:01:58 -0700

    added game logic to new file, fixed grid

---
+++ game.js
@@ -0,0 +1,58 @@
+// variables
+const cols = 10;
+const rows = 10;
+
+let money = 3830;
+let seeds = []; // makes queue of seeds
+
+
+const fieldDiv = document.getElementById('field');
+const moneyDisplay = document.getElementById('money-display');
+
+const field = Array.from({length: cols}, () => Array.from({length: rows}, () => null));
+
+function updateMoney(){
+  moneyDisplay.textContent = `Money: $${money}`;
+}
+
+function renderField(){
+  fieldDiv.innerHTML = ''; //clears existing grid
+
+  // makes grid :)
+  for (let col = 0; col < cols; col++) {
+    const colDiv = document.createElement('div');
+    colDiv.classList.add('col');
+
+    for (let row = 0; row < rows; row++) {
+      const cellDiv = document.createElement('div');
+      cellDiv.classList.add('cell');
+
+      const plant = field[col][row];
+      if (plant) {
+        if (plant.fire) {
+          cellDiv.classList.add('fire');
+        }
+        else {
+          const img = document.createElement('img');
+          img.src = 
+          'plant/${plant.type}-${plant.stage}.png';
+          cellDiv.appendChild(img);
+        }
+      }
+      // plants a seed when you click an empty cell 
+      cellDiv.onclick = () => {
+        if (!field[col][row] && seeds.length > 0){
+          field[col][row] = seeds.pop();
+          renderField();
+        }
+      };
+    
+    colDiv.appendChild(cellDiv);
+  }
+  fieldDiv.appendChild(colDiv);
+  }
+}
+
+//
+renderField();
+updateMoney();
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron/main')
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -12,54 +12,3 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow()
 })
-// variables
-const cols = 10
-const rows = 10
-let seeds = []; // makes queue of seeds
-const fieldDiv = document.getElementById('field');
-const moneyDisplay = document.getElementById('money-display');
-function updateMoney(){
-  moneyDisplay.textContent = `Money: $$(Money)`;
-}
-
-function renderField(){
-  fieldDiv.innerHTML = ''; //clears existing grid
-
-  // makes grid :)
-  for (let col = 0; col < cols; col++) {
-    const colDiv = document.createElement('div');
-    colDiv.classlist.add('column');
-
-    for (let row = 0; row < rows; row++) {
-      const cellDiv = document.createElement('div');
-      cellDiv.classList.add('cell');
-
-      const plant = field[col][row];
-      if (plant) {
-        if (plant.fire) {
-          cellDiv.classList.add('fire');
-        }
-        else {
-          const img = document.createElement('img');
-          img.src = 
-          'plant/${plant.type}-${plant.stage}.png';
-          cellDiv.appendChild(img);
-        }
-      }
-      // plants a seed when you click an empty cell 
-      cellDiv.onclick = () => {
-        if (!field[col][row] && seeds.length > 0){
-          field[col][row] = seeds.pop();
-          renderField();
-        }
-      };
-    
-    colDiv.appendChild(cellDiv);
-  }
-  fieldDiv.apendChild(colDiv);
-  }
-}
-
-//
-renderField();
-updateMoney();
